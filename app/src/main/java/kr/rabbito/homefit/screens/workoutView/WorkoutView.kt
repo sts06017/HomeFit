c71a6c919e5533b28c81ec45bdda5c138f6b4e4d
최승호 <sts06017@naver.com>
2023-06-21 22:28:34 +0900

    WODetail - 운동시 세트당 횟수 적용 구현

--- app/src/main/java/kr/rabbito/homefit/screens/workoutView/WorkoutView.kt
+++ app/src/main/java/kr/rabbito/homefit/screens/workoutView/WorkoutView.kt
@@ -7,7 +7,7 @@ import kr.rabbito.homefit.workout.WorkoutState
 abstract class WorkoutView(val context: Context, val binding: ActivityWoBinding) {
     open fun generateWidgets() {}
     fun refreshValues() {
-        binding.woTvCount.text = "${WorkoutState.count}/15"
+        binding.woTvCount.text = "${WorkoutState.count}/${WorkoutState.setCondition}"
         binding.woTvSet.text = "${WorkoutState.set}/${WorkoutState.setTotal}"
     }
 }
