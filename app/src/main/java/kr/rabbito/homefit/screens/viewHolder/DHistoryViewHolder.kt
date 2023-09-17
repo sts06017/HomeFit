bdfd2a389b7d4f66c78884dc837acabafa2effd9
ThinkingDobby <chiho1221@naver.com>
2023-09-17 17:04:06 +0900

    식단 달력 복귀 시 갱신되도록 수정

--- app/src/main/java/kr/rabbito/homefit/screens/viewHolder/DHistoryViewHolder.kt
+++ app/src/main/java/kr/rabbito/homefit/screens/viewHolder/DHistoryViewHolder.kt
@@ -1,5 +1,6 @@
 package kr.rabbito.homefit.screens.viewHolder
 
+import android.app.Activity
 import android.content.Intent
 import android.util.Log
 import android.view.View
@@ -36,6 +37,7 @@ class DHistoryViewHolder(private val binding: DreportItemBinding)
             intent.putExtra("DATE", diet!!.dDate.toString())
 
             itemView.context.startActivity(intent)
+            (itemView.context as Activity).finish()
         }
     }
 }
